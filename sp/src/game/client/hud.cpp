143d550d9aeeb1c66b3ffe64615479c8c96e941e
sabianroberts <djdise@outlook.com>
2021-05-19 16:54:05 +0100

    Revert "Add version"
    This reverts commit 66990b81150491925ca9335c26aa46a6d3088fcd.

--- sp/src/game/client/hud.cpp
+++ sp/src/game/client/hud.cpp
@@ -26,7 +26,6 @@
 #include <vgui_controls/AnimationController.h>
 #include <vgui/ISurface.h>
 #include "hud_lcd.h"
-#include "version_info.h"
 
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
@@ -393,22 +392,6 @@ CHud::CHud()
 //-----------------------------------------------------------------------------
 // Purpose: This is called every time the DLL is loaded
 //-----------------------------------------------------------------------------
-void PrintVersion(const CCommand &args)
-{
-	if (args.ArgC() < 1 || args.Arg(1) == "")
-	{
-		DevMsg("\n||------------------------------------------||"); // Sourced from PhoenixADM and Half-Life ADM by The Phoenix Project Software and Admer456.
-		DevMsg("\n  Client DLL build %s (%s)", gameBuild, gameDate);
-		DevMsg("\n  Source Anniversary 2013 %s", gameVersion);
-		DevMsg("\n  Author(s): %s", gameAuthors);
-		DevMsg("\n  Release type: %s", gameRelease);
-		DevMsg("\n  Repository: %s", gameRepo);
-		DevMsg("\n||------------------------------------------||");
-		return;
-	}
-
-}
-
 void CHud::Init( void )
 {
 	HOOK_HUD_MESSAGE( gHUD, ResetHUD );
@@ -486,8 +469,6 @@ void CHud::Init( void )
 	}
 
 	FreeHudTextureList( textureList );
-
-	// ConCommand version_anniversary("version_anniversary", PrintVersion, "Prints the version", 0);
 }
 
 //-----------------------------------------------------------------------------
