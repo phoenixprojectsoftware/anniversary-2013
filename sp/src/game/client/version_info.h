40c83db9068b8b107975f93e200bb27ae27ba74a
sabianroberts <djdise@outlook.com>
2021-05-22 14:33:48 +0100

    Add Multiplayer

---
+++ sp/src/game/client/version_info.h
@@ -0,0 +1,15 @@
+/*
+<¦=============================================================¦>
+Source Anniversary 2013
+Written by Sabian Roberts for The Phoenix Project Software
+<¦=============================================================¦>
+*/
+
+// Special thanks to Admer
+// Code below originally sourced from PhoenixADM
+#define gameBuild "2105-rev1"
+#define gameDate __DATE__
+#define gameVersion "1.0 (November 2024)"
+#define gameRelease "Private - for developers and playtesters at The Phoenix Project Software only"
+#define gameAuthors "The Phoenix Project Software"
+#define gameRepo "https://github.com/phoenixprojectsoftware/anniversary-2013"
